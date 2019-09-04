ea6ddc885f5db13db9fe6aa1dad4719c467321f3
thomas-armena <thomasarmena@gmail.com>
2019-09-03 22:32:00 -0400

    first commit

---
+++ precache-manifest.0d6caaf88dd79de6be4321db29707668.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384cd2ef9eb00ac079eecbc317c07467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92f0ab10ed235f51fb13",
+    "url": "/static/css/main.33e71c4d.chunk.css"
+  },
+  {
+    "revision": "b50dae8e8d3db7a0a894",
+    "url": "/static/js/2.7235e155.chunk.js"
+  },
+  {
+    "revision": "92f0ab10ed235f51fb13",
+    "url": "/static/js/main.3d46b65e.chunk.js"
+  },
+  {
+    "revision": "737425d3abf597188c5e",
+    "url": "/static/js/runtime~main.46ff90e1.js"
+  }
+]);
